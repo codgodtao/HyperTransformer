52d3085347eacfa81bf00ad94115c937d7c3e1f2
codgodtao <321227312@qq.com>
2023-05-27 22:25:23 +0800

    add

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Pansharpening is a process of merging a highresolution panchromatic (PAN) image and a low-resolution multispectral (LRMS) image to create a single high-resolution multispectral (HRMS) image.
 # What we have done?
 our code is based on HyperTransformer(CVPR 2022), which is used for Hyperspectral pansharpening
-For more information, please see
 - **Paper**: [`CVPR-2022-Open-Access`](https://openaccess.thecvf.com/content/CVPR2022/html/Bandara_HyperTransformer_A_Textural_and_Spectral_Feature_Fusion_Transformer_for_Pansharpening_CVPR_2022_paper.html) or [`arxiv`](https://arxiv.org/abs/2203.02503).
 1. 原论文是做高光谱图像融合的，我们拓展到多光谱全色锐化领域，两个任务不同，数据的尺度、通道维度等不一样，我们修改了指标评估代码和训练推理的流程上一些适配性的问题，并提供了新的训练好的模型和结果（WV4数据）；
 2. 原论文的backbone收敛很慢，我们替换为预训练的RESnet50，并为了将RGB上训练的res50模型应用到遥感图像，在网络前后增加了一些层来修改通道，只使用其前半部分结构等，具体在models/HyperTransformer的979行后面 
@@ -16,11 +15,9 @@ For more information, please see
 
 
 # Data Flow chart
-
-
-
-
-
+![alt DataSet](imgs/dataset.jpg "数据集格式")
+![alt Flowchart](imgs/flowchart.jpg "流程图")
+![alt Result](imgs/result.jpg "tensorboard的测试结果图")
 
 # Setting up a virtual conda environment
 Setup a virtual conda environment using the provided ``requirements.txt``.
@@ -34,7 +31,7 @@ We use multi-spectral pansharpening dataset acquired by WorldView4(WV4),QuickBir
 The number of bands in WV4 and QB is 4, and WV2 is 8
 1) **WV4 WV2 QB** [`Download the .h5 file here`](https://pan.baidu.com/s/1ZdIHrQB93-ASi2zR5kJz0w?pwd=dffs)
 You need ot updata the path and spctral bands in configs/config_HSIT_PRE.json for crossponding datasets.
-
+Beside, you need to edit 144 line in dataloader/HSI_dataset.py for your data path.
 # Trained models and pansharpened results on test-set
 You can download trained models and final prediction outputs through the follwing links for each dataset.
  1) WV4: [`Download here`](https://pan.baidu.com/s/1rtO6g39PWOeK7kD0cqkrIw?pwd=qf3q)
@@ -59,7 +56,13 @@ Use the following codes to train HyperTransformer on the four datasets.
     2) Then use following commad to train on Pavia Center dataset. 
     `python train.py --config configs/config_HSIT_PRE.json`.
 
-
 # Experiment Result
-We compared the this method with multiple classic pansharpening methods, and the results showed that this method is a qualified Pansharpening solution.
+ The results on WV4 dataset showed that this method is a qualified Pansharpening solution.
 
+|  MetricsMethod | Ours | Original | Pannet    |
+| :-----| ----: | :----: |-----|
+|  CC  | 0.95| 0.88  | 0.94  |
+|  SAM  | 1.88  | 2.30 |1.62  |
+|  RMSE  | 0.02  | 0.06 |0.03  |
+|  ERGAS  | 0.38  | 0.48 |0.34  |
+|  PSNR  | 32.30  | 28.31 |31.40  |
--- gradio_demo.py
+++ gradio_demo.py
@@ -25,63 +25,69 @@ Image.fromarray(data.cat()).save('3.jpeg')
 
 
 
-def detect(img):
+def detect(img,img2):
     if isinstance(img, str): # 这里需要改成pansharpening的图像格式
         img = get_url_img(img) if img.startswith('http') else Image.open(img).convert('RGB')
-    result = model.predict(source=img)#模型结果
-    vis = img
-    return vis
+    if isinstance(img2, str): # 这里需要改成pansharpening的图像格式
+        img2 = get_url_img(img2) if img2.startswith('http') else Image.open(img2).convert('RGB')
+    # result = model.predict(source=img)#模型结果
+    print(img,img2)
+    result = model(img, img2)
+    return result
 
 
 with gr.Blocks() as demo:
     gr.Markdown("# Pansharpening可视化测试")
 
-    with gr.Tab("捕捉摄像头"):
-        in_img = gr.Image(source='webcam', type='pil')
-        button = gr.Button("执行", variant="primary")
-
-        gr.Markdown("## 输出")
-        out_img = gr.Image(type='pil')
-
-        button.click(detect,
-                     inputs=in_img,
-                     outputs=out_img)
+    # with gr.Tab("捕捉摄像头"):
+    #     in_img = gr.Image(source='webcam', type='pil')
+    #     button = gr.Button("执行", variant="primary")
+    #
+    #     gr.Markdown("## 输出")
+    #     out_img = gr.Image(type='pil')
+    #
+    #     button.click(detect,
+    #                  inputs=in_img,
+    #                  outputs=out_img)
 
     with gr.Tab("选择示例图片"):
         files = ['1.jpeg', '2.jpeg', '3.jpeg']
         drop_down = gr.Dropdown(choices=files, value=files[0])
         button = gr.Button("执行", variant="primary")
+        drop_down2 = gr.Dropdown(choices=files, value=files[1])
 
         gr.Markdown("## 输出")
         out_img = gr.Image(type='pil')
 
         button.click(detect,
-                     inputs=drop_down,
+                     inputs=[drop_down,drop_down2],
                      outputs=out_img)
 
     with gr.Tab("输入图片链接"):
         default_url = ''#网页链接
         url = gr.Textbox(value=default_url)
+        url2 = gr.Textbox(value=default_url)
         button = gr.Button("执行", variant="primary")
 
         gr.Markdown("## 输出")
         out_img = gr.Image(type='pil')
 
         button.click(detect,
-                     inputs=url,
+                     inputs=[url,url2],
                      outputs=out_img)
 
     with gr.Tab("上传本地图片"):
         input_img = gr.Image(type='pil')
+        input_img2 = gr.Image(type='pil')
         button = gr.Button("执行检测", variant="primary")
 
         gr.Markdown("## 预测输出")
         out_img = gr.Image(type='pil')
 
         button.click(detect,
-                     inputs=input_img,
+                     inputs=[input_img,input_img2],
                      outputs=out_img)
 
 gr.close_all()
-demo.queue(concurrency_count=5)
-demo.launch()
+demo.queue(concurrency_count=3)
+demo.launch(share=True)
